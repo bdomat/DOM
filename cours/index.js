80cce5f36b6fa98ed9fd301637ea5846380244ec
bdomat <bertrand.domat@gmail.com>
2023-01-10 22:48:34 +0100

    events-p2

--- cours/index.js
+++ cours/index.js
@@ -24,4 +24,91 @@ btn2.addEventListener("click", () => {
   response.style.background = "red";
 });
 
+/* PRIORITE: <div> > #id > .class > baliseHTML */
 //----------------------------------------------------------
+
+//Mouse Events
+
+const mousemove = document.querySelector(".mousemove");
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+});
+
+window.addEventListener("mouseup", () => {
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+  mousemove.style.border = "2px solid teal";
+});
+
+questionContainer.addEventListener("mouseenter", () => {
+  questionContainer.style.background = "rgba(0,0,0,0.6)";
+});
+
+questionContainer.addEventListener("mouseout", () => {
+  questionContainer.style.background = "rgba(87,65,0,1)";
+});
+response.addEventListener("mouseover", () => {
+  response.style.transform = "rotate(3deg)";
+});
+response.addEventListener("mouseout", () => {
+  response.style.transform = "rotate(0deg)";
+});
+
+//----------------------------------------------------------
+
+//KeyPress event
+const keypressContainer = document.querySelector(".keypress");
+const key = document.getElementById("key");
+const ring = (key) => {
+  const audio = new Audio();
+  audio.src = key + ".mp3";
+  console.log(audio.src);
+  audio.play();
+};
+
+document.addEventListener("keypress", (e) => {
+  key.textContent = e.key;
+
+  if (e.key === "j") {
+    keypressContainer.style.background = "pink";
+  } else if (e.key === "h") {
+    keypressContainer.style.background = "teal";
+  } else {
+    keypressContainer.style.background = "red";
+  }
+  if (e.key === "z") ring(e.key);
+});
+
+// --------------------------------
+// Scroll Event
+
+const nav = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 120) {
+    nav.style.top = 0;
+  } else {
+    nav.style.top = "-50px";
+  }
+});
+
+//---------------------------------
+// Form Events
+const inputName = document.querySelector('input[type="text"]');
+const select = document.querySelector("select");
+let pseudo = "";
+let language = "";
+
+inputName.addEventListener("input", (e) => {
+  pseudo = e.target.value;
+});
+
+select.addEventListener("input", (e) => {
+  language = e.target.value;
+});
+
+// video : 2h16m22s
