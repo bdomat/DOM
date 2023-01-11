062d151d771c37e68eb254ddfc5b68e126f310d9
Bertrand Domat <bertrand.domat@gmail.com>
2023-01-11 18:21:41 +0100

    fin 2/2

--- cours/index.js
+++ cours/index.js
@@ -100,6 +100,7 @@ window.addEventListener("scroll", () => {
 // Form Events
 const inputName = document.querySelector('input[type="text"]');
 const select = document.querySelector("select");
+const form = document.querySelector("form");
 let pseudo = "";
 let language = "";
 
@@ -111,4 +112,156 @@ select.addEventListener("input", (e) => {
   language = e.target.value;
 });
 
-// video : 2h16m22s
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (cgv.checked) {
+    //Affiche le contenu des varibales
+    document.querySelector("form > div").innerHTML = `
+    <h3>Pseudo : ${pseudo}</h3>
+    <h4>Langage préféré : ${language}</h4>
+    `;
+  } else {
+    alert("Veuillez accepter les CGV");
+  }
+});
+
+//--------------------------
+
+//Load event
+window.addEventListener("load", () => {
+  console.log("Document chargé !");
+});
+//----------------------------
+// ForEach
+const boxes = document.querySelectorAll(".box");
+console.log(boxes);
+boxes.forEach((box) => {
+  box.addEventListener("click", (e) => {
+    e.target.style.transform = "scale(0.7)";
+  });
+});
+//---------------------------------------
+// addEventListener Vs onclick
+
+// document.body.onclick = () => {
+//   console.log("click !");
+// };
+// document.body.onclick = () => {
+//   console.log("scroll !");
+// };
+// = !! on ne peut pas propager 2 fois le meme evenement sur une balise avec cette méthode contrairement à addEventListener
+
+//Bubbling => fin (de base l'eventlistener est paramétré en mode Bubbling)
+document.body.addEventListener(
+  "click",
+  () => {
+    console.log("clik 1!");
+  },
+  false
+);
+
+//Usecapture
+document.body.addEventListener(
+  "click",
+  () => {
+    console.log("click 2!");
+  },
+  true
+);
+
+//----------------------------------------------
+//Stop propagation
+questionContainer.addEventListener("click", (e) => {
+  alert("Test !");
+  e.stopPropagation();
+});
+
+//removEventListener
+
+//-----------------------------------------------
+// BOM
+
+// console.log(window.innerHeight);
+// console.log(window.scrollY);
+//window.open("http://google.com", "cours js", "height=600", "width=800");
+//window.close()
+
+//Evénements adossés à Window
+//alert("hello");
+
+//confirm
+btn2.addEventListener("click", () => {
+  confirm("Voulez vous vraiment vous tromper ?");
+});
+
+// prompt
+btn1.addEventListener("click", () => {
+  let answer = prompt("Entrez votre nom !");
+  questionContainer.innerHTML = "<h3>Bravo " + answer + "</h3";
+});
+
+//
+
+setTimeout(() => {
+  //logique à éxecuter
+  questionContainer.style.borderRadius = "300px";
+}, 2000);
+
+// let interval = setInterval(() => {
+//   document.body.innerHTML += "<div class='box'><h2>Nouvelle Boite !</h2></div>";
+// }, 1000);
+
+// document.body.addEventListener("click", (e) => {
+//   e.target.remove();
+//   clearInterval(interval);
+// });
+
+//Location
+// console.log(location.href);
+// console.log(location.host);
+// console.log(location.pathname);
+// console.log(location.search);
+// location.replace("http://lequipe.fr");
+
+// window.onload = () => {
+//   location.href = "https://bd-web.fr";
+// };
+
+//Navigator
+// console.log(navigator.userAgent);
+// https://developer.mozilla.org/fr/docs/Web/API/Geolocation/getCurrentPosition
+
+/*var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log("Votre position actuelle est :");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude : ${crd.longitude}`);
+  console.log(`La précision est de ${crd.accuracy} mètres.`);
+}
+
+function error(err) {
+  console.warn(`ERREUR (${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);*/
+
+//History
+
+// console.log(window.history);
+// window.history.back()
+// history.go(-2)
+//----------------------
+
+//SetProperty----------------------------
+window.addEventListener("mousemove", (e) => {
+  nav.style.setProperty("--x", e.layerX + "px");
+  nav.style.setProperty("--y", e.layerY + "px");
+});
